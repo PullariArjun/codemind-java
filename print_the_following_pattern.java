3da83329cb65babe1a28399b75d44f3b5b36ac27
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-04-29 19:37:07 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         int a,i,j;         Scanner sc= new Scanner(System.in);         a=sc.nextInt();         for(i=1;i<=a;i++)         {             for(j=1;j<=a;j++)             {                 if(i==j)                 {                     System.out.print("0");                 }                 else                 {                     System.out.print("x");                 }
                }
                System.out.print("
    ");
            }
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,13 +4,21 @@ class arjun
     public static void main(String ram[])
     {
         int a,i,j;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         a=sc.nextInt();
-        for(i=a;i>0;i--)
+        for(i=1;i<=a;i++)
         {
-            for(j=1;j<=i;j++)
+            for(j=1;j<=a;j++)
             {
-                System.out.print(j);
+                if(i==j)
+                {
+                    System.out.print("0");
+                }
+                else
+                {
+                    System.out.print("x");
+                }
+    
             }
             System.out.print("
 ");
