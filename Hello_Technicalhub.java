04caee9a7fda53766fd67bcdfa5db2caf0909cc1
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-05-17 19:25:14 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         String a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine();         System.out.print("Hello Technicalhub "+a);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        String a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        System.out.print("Hello Technicalhub
+"+a);
+    }
+}
