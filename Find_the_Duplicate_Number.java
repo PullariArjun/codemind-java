f314c5bab30fca81f3e2a911e2726063183ce065
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-06-07 17:45:38 +0530

    import java.util.Scanner; class arjun {     public void fdn(int n,int arr[])     {         int i,j;         for(i=0;i<n;i++)         {             int count=0;             for(j=0;j<n;j++)             {                 if(arr[i]==arr[j])                 {                     count++;                 }             }             if(count==2)             {                 System.out.print(arr[i]);                 arr[i]=-1;             }         }     }     public static void main(String ram[])     {         int n,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         arjun a=new arjun();         a.fdn(n,arr);     } }

---
+++ Find_the_Duplicate_Number.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class arjun
+{
+    public void fdn(int n,int arr[])
+    {
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+            int count=0;
+            for(j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    count++;
+                }
+            }
+            if(count==2)
+            {
+                System.out.print(arr[i]);
+                arr[i]=-1;
+            }
+        }
+    }
+    public static void main(String ram[])
+    {
+        int n,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        arjun a=new arjun();
+        a.fdn(n,arr);
+    }
+}
