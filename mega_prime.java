5a4e6ecac02a84c7c0a753eb5bd520b4b03aadb5
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-06-22 11:58:59 +0530

    import java.util.Scanner; class arjun {     public void mp(int n)     {         int i,c=0;         int r;         for(i=1;i<=n;i++)         {             if(n%i==0)             {                c++;             }         }         if(c==2)         {             int temp=n;             int count1=0;             while(temp>0)             {                 temp=temp/10;                 count1++;             }             int count2=0;             while(n>0)             {                 c=0;                 r=n%10;                 for(i=1;i<=r;i++)                 {                     if(r%i==0)                     {                         c++;                     }                 }                 if(c==2)                 {                    count2++;                 }                 n=n/10;             }             if(count1==count2)             System.out.print("Mega Prime");             else             System.out.print("Not Mega Prime");         }         else         System.out.print("Not Mega Prime");     }     public static void main(String ram[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         arjun ar=new arjun();         ar.mp(n);     } }

---
+++ mega_prime.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+class arjun
+{
+    public void mp(int n)
+    {
+        int i,c=0;
+        int r;
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+               c++;
+            }
+        }
+        if(c==2)
+        {
+            int temp=n;
+            int count1=0;
+            while(temp>0)
+            {
+                temp=temp/10;
+                count1++;
+            }
+            int count2=0;
+            while(n>0)
+            {
+                c=0;
+                r=n%10;
+                for(i=1;i<=r;i++)
+                {
+                    if(r%i==0)
+                    {
+                        c++;
+                    }
+                }
+                if(c==2)
+                {
+                   count2++;
+                }
+                n=n/10;
+            }
+            if(count1==count2)
+            System.out.print("Mega Prime");
+            else
+            System.out.print("Not Mega Prime");
+        }
+        else
+        System.out.print("Not Mega Prime");
+    }
+    public static void main(String ram[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        arjun ar=new arjun();
+        ar.mp(n);
+    }
+}
