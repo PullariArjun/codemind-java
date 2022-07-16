4fcd613c569076b66d8de8027b23b1409e91363b
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-07-16 14:40:32 +0530

    import java.util.Scanner; class arjun {     public static void fab(int n)     {         int i=0,j=1,sum=0,x=0,flag=0;         while(sum<n)         {             sum=i+j;             i=j;             j=sum;             x++;             if(sum==n)             {                 flag=1;             }         }         if(flag==1)         System.out.print("True");         else         System.out.print("False");     }     public static void main(String ram[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         arjun.fab(n);     } }

---
+++ Fibonacci_or_not.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void fab(int n)
+    {
+        int i=0,j=1,sum=0,x=0,flag=0;
+        while(sum<n)
+        {
+            sum=i+j;
+            i=j;
+            j=sum;
+            x++;
+            if(sum==n)
+            {
+                flag=1;
+            }
+        }
+        if(flag==1)
+        System.out.print("True");
+        else
+        System.out.print("False");
+    }
+    public static void main(String ram[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        arjun.fab(n);
+    }
+}
