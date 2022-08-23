3132fd06b3faaae2992b2cfdc362ada1d8b55967
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-08-23 15:51:55 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         int l,w,h,n,i,j;         Scanner sc=new Scanner(System.in);         l=sc.nextInt();         n=sc.nextInt();         int arr[][]=new int[n][2];         for(i=0;i<n;i++)         {             for(j=0;j<2;j++)             {                 arr[i][j]=sc.nextInt();             }             if(arr[i][0]<l||arr[i][1]<l)             System.out.print("UPLOAD ANOTHER ");             else if(arr[i][0]==arr[i][1])             System.out.print("ACCEPTED ");             else             System.out.print("CROP IT ");         }     } }

---
+++ Roy_and_Profile_Picture.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        int l,w,h,n,i,j;
+        Scanner sc=new Scanner(System.in);
+        l=sc.nextInt();
+        n=sc.nextInt();
+        int arr[][]=new int[n][2];
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<2;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+            if(arr[i][0]<l||arr[i][1]<l)
+            System.out.print("UPLOAD ANOTHER
+");
+            else if(arr[i][0]==arr[i][1])
+            System.out.print("ACCEPTED
+");
+            else
+            System.out.print("CROP IT
+");
+        }
+    }
+}
