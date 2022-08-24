80e9528c0c2df94d75451f3aa61e02c01388ae24
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-08-24 14:29:11 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         int n,i,count1=0,count2=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();             if(arr[i]%2==0)             count1++;             if(i%2==0&&arr[i]%2==0)             count2++;         }         if(count1==count2)         System.out.print("True");         else         System.out.print("False");     } }

---
+++ Strictly_EVEN.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        int n,i,count1=0,count2=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(arr[i]%2==0)
+            count1++;
+            if(i%2==0&&arr[i]%2==0)
+            count2++;
+        }
+        if(count1==count2)
+        System.out.print("True");
+        else
+        System.out.print("False");
+    }
+}
