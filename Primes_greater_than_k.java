f8c9b09e6b520cd75366a0c0f3296ac5e78f257d
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-08-30 17:57:04 +0530

    import java.util.Scanner; class arjun {     public int prime(int n)     {         int i,count=0;         for(i=1;i<=n;i++)         {             if(n%i==0)             {                 count++;             }         }         return count;     }     public static void main(String ram[])     {         int n,k,i,count=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         arjun ar=new arjun();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(ar.prime(arr[i])==2&&arr[i]>=k)             count++;         }         System.out.print(count);     } }

--- Primes_greater_than_k.java
+++ Primes_greater_than_k.java
@@ -1,11 +1,24 @@
 import java.util.Scanner;
 class arjun
 {
+    public int prime(int n)
+    {
+        int i,count=0;
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
     public static void main(String ram[])
     {
-        int n,k,i,j,primes_grater_k=0;
+        int n,k,i,count=0;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
+        arjun ar=new arjun();
         int arr[]=new int[n];
         for(i=0;i<n;i++)
         {
@@ -13,19 +26,10 @@ class arjun
         }
         k=sc.nextInt();
         for(i=0;i<n;i++)
-        {int count=0;
-            for(j=2;j<arr[i];j++)
-            {
-                if(arr[i]%j==0)
-                {
-                    count++;
-                }
-            }
-            if(count==0&&arr[i]>=k)
-            {
-                primes_grater_k++;
-            }
+        {
+            if(ar.prime(arr[i])==2&&arr[i]>=k)
+            count++;
         }
-        System.out.print(primes_grater_k);
+        System.out.print(count);
     }
 }
