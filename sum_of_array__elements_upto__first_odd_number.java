1ad45af47d4f9eaf6a020ad20dba5c8894208864
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-08-30 16:24:45 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         int n,i,sum=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();             if(arr[i]%2==0)             sum=sum+arr[i];             else             break;         }         System.out.print(sum);     } }

---
+++ sum_of_array__elements_upto__first_odd_number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        int n,i,sum=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(arr[i]%2==0)
+            sum=sum+arr[i];
+            else
+            break;
+        }
+        System.out.print(sum);
+    }
+}
