23af0bbb7be2209a3f6cddb5820cee998a8cfd59
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-08-30 15:15:34 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         int n,i,j,f=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             int count=1;             if(arr[i]==-1)             continue;             for(j=0;j<n;j++)             {                 if(arr[i]==arr[j]&&i!=j)                 {                     count++;                     arr[j]=-1;                 }             }             if(arr[i]==count)             {                 f++;             }         }         System.out.print(f);     } }

---
+++ value_same_as_count.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        int n,i,j,f=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            int count=1;
+            if(arr[i]==-1)
+            continue;
+            for(j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j]&&i!=j)
+                {
+                    count++;
+                    arr[j]=-1;
+                }
+            }
+            if(arr[i]==count)
+            {
+                f++;
+            }
+        }
+        System.out.print(f);
+    }
+}
