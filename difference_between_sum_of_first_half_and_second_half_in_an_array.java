da67456771c2b89ab9609be611f744f69573be04
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-08-30 16:38:28 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         int n,i,sum1=0,sum2=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();             if(i<n/2)             sum1=sum1+arr[i];             else             sum2=sum2+arr[i];         }         if(sum1>sum2)         System.out.print(sum1-sum2);         else         System.out.print(sum2-sum1);     } }

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        int n,i,sum1=0,sum2=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(i<n/2)
+            sum1=sum1+arr[i];
+            else
+            sum2=sum2+arr[i];
+        }
+        if(sum1>sum2)
+        System.out.print(sum1-sum2);
+        else
+        System.out.print(sum2-sum1);
+    }
+}
