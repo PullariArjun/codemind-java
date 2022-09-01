96cb8a0dd58c62aae155a05ab8762276bb60f8f8
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-09-01 11:25:12 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         int n,m,i,j;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         m=sc.nextInt();         int arr1[]=new int[n];         int arr2[]=new int[m];         for(i=0;i<n;i++)         arr1[i]=sc.nextInt();         for(i=0;i<m;i++)         arr2[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(arr1[i]==-1)             continue;             for(j=0;j<n;j++)             {                 if(arr1[i]==arr1[j]&&i!=j)                 arr1[j]=-1;             }         }         for(i=0;i<m;i++)         {             if(arr2[i]==-1)             continue;             for(j=0;j<m;j++)             {                 if(arr2[i]==arr2[j]&&i!=j)                 arr2[j]=-1;             }         }         for(i=0;i<n;i++)         {int c=0;             if(arr1[i]==-1)             continue;             for(j=0;j<m;j++)             {                 if(arr1[i]==arr2[j])                 c++;             }             if(c==0)             System.out.print(arr1[i]+" ");         }         for(i=0;i<m;i++)         {   int c=0;             if(arr2[i]==-1)             continue;             for(j=0;j<n;j++)             {                 if(arr2[i]==arr1[j])                 c++;             }             if(c==0)             System.out.print(arr2[i]+" ");         }     } }

--- Common_elements_II.java
+++ Common_elements_II.java
@@ -3,43 +3,59 @@ class arjun
 {
     public static void main(String ram[])
     {
-        int m,n,i,j;
+        int n,m,i,j;
         Scanner sc=new Scanner(System.in);
-        m=sc.nextInt();
         n=sc.nextInt();
-        int arr1[]=new int[m];
-        int arr2[]=new int[n];
+        m=sc.nextInt();
+        int arr1[]=new int[n];
+        int arr2[]=new int[m];
+        for(i=0;i<n;i++)
+        arr1[i]=sc.nextInt();
         for(i=0;i<m;i++)
+        arr2[i]=sc.nextInt();
+        for(i=0;i<n;i++)
         {
-            arr1[i]=sc.nextInt();
-        }
-        for(j=0;j<n;j++)
-        {
-            arr2[j]=sc.nextInt();
-        }
-        for(i=0;i<m;i++)
-        {int count=0;
+            if(arr1[i]==-1)
+            continue;
             for(j=0;j<n;j++)
             {
-                if(arr1[i]==arr2[j])
-                {
-                    count++;
-                }
+                if(arr1[i]==arr1[j]&&i!=j)
+                arr1[j]=-1;
             }
-            if(count==0)
-            System.out.print(arr1[i]+" ");
         }
-        for(j=0;j<n;j++)
-        {int count=0;
-            for(i=0;i<m;i++)
+        for(i=0;i<m;i++)
+        {
+            if(arr2[i]==-1)
+            continue;
+            for(j=0;j<m;j++)
+            {
+                if(arr2[i]==arr2[j]&&i!=j)
+                arr2[j]=-1;
+            }
+        }
+        for(i=0;i<n;i++)
+        {int c=0;
+            if(arr1[i]==-1)
+            continue;
+            for(j=0;j<m;j++)
             {
                 if(arr1[i]==arr2[j])
-                {
-                    count++;
-                }
+                c++;
             }
-            if(count==0)
-            System.out.print(arr2[j]+" ");
+            if(c==0)
+            System.out.print(arr1[i]+" ");
+        }
+        for(i=0;i<m;i++)
+        {   int c=0;
+            if(arr2[i]==-1)
+            continue;
+            for(j=0;j<n;j++)
+            {
+                if(arr2[i]==arr1[j])
+                c++;
+            }
+            if(c==0)
+            System.out.print(arr2[i]+" ");
         }
     }
 }
