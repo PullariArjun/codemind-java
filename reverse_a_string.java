e780946d9dcb8c0e5e893cbf3e1dc05cc086905b
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-09-02 18:32:20 +0530

    import java.util.*; class arjun {     public static void main(String ram[])     {         String hi;         Scanner sc=new Scanner(System.in);         hi=sc.nextLine();         StringBuffer sb=new StringBuffer(hi);         System.out.print(sb.reverse());     } }

---
+++ reverse_a_string.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        String hi;
+        Scanner sc=new Scanner(System.in);
+        hi=sc.nextLine();
+        StringBuffer sb=new StringBuffer(hi);
+        System.out.print(sb.reverse());
+    }
+}
