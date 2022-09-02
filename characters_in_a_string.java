d0d8f59e036bbc38e7babc19685c4b9799a616ef
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-09-02 18:21:49 +0530

    import java.util.*; class arjun {     public static void main(String ram[])     {         String n;         Scanner sc=new Scanner(System.in);         n=sc.nextLine();         System.out.print(n.length());     } }

---
+++ characters_in_a_string.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        String n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextLine();
+        System.out.print(n.length());
+    }
+}
