126e500bfdfec895c386a2e9a03225d2702315b7
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-09-02 12:59:07 +0530

    import java.util.*; class arjun {     public static void main(String ram[])     {         int n,k,i,count=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         k=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();             if(arr[i]%k!=0)             count++;         }         System.out.print(count);     } }

--- Count_elements_not_divisible_by_K.java
+++ Count_elements_not_divisible_by_K.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 class arjun
 {
     public static void main(String ram[])
     {
-        int n,k,count=0,i;
+        int n,k,i,count=0;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         k=sc.nextInt();
@@ -11,13 +11,8 @@ class arjun
         for(i=0;i<n;i++)
         {
             arr[i]=sc.nextInt();
-        }
-        for(i=0;i<n;i++)
-        {
             if(arr[i]%k!=0)
-            {
-                count=count+1;
-            }
+            count++;
         }
         System.out.print(count);
     }
