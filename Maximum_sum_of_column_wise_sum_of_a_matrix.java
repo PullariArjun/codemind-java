5cb71cbb341dbd5a7190aa1597ddd82d9a8a0ce5
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-09-04 10:38:30 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         int n,m,i,j,max=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         m=sc.nextInt();         int arr[][]=new int[n][m];         for(i=0;i<n;i++)         {             for(j=0;j<m;j++)             {                 arr[i][j]=sc.nextInt();             }         }         for(i=0;i<m;i++)         {int sum=0;             for(j=0;j<n;j++)             {                 sum=sum+arr[j][i];             }             if(max<sum)             max=sum;         }         System.out.print(max);     } }

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        int n,m,i,j,max=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        int arr[][]=new int[n][m];
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(i=0;i<m;i++)
+        {int sum=0;
+            for(j=0;j<n;j++)
+            {
+                sum=sum+arr[j][i];
+            }
+            if(max<sum)
+            max=sum;
+        }
+        System.out.print(max);
+    }
+}
