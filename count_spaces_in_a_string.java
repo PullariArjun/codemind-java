5e59429ec01f6775ce2b5cc1a432b96a25d34f64
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-09-29 19:52:50 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         String str;         Scanner sc=new Scanner(System.in);         str=sc.nextLine();         char ch[]=str.toCharArray();         int a=0;         for(int i=0;i<str.length();i++)         {             if(ch[i]==' ')             a++;         }         System.out.print(a);     } }

---
+++ count_spaces_in_a_string.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        String str;
+        Scanner sc=new Scanner(System.in);
+        str=sc.nextLine();
+        char ch[]=str.toCharArray();
+        int a=0;
+        for(int i=0;i<str.length();i++)
+        {
+            if(ch[i]==' ')
+            a++;
+        }
+        System.out.print(a);
+    }
+}
