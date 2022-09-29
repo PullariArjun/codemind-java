3fe03c0494d334e5c5a79ecbeda317f6c3657006
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-09-29 22:25:14 +0530

    import java.util.Scanner; class arjun {     public static void main(String ram[])     {         String str;         Scanner sc=new Scanner(System.in);         str=sc.nextLine();         System.out.print(str.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class arjun
+{
+    public static void main(String ram[])
+    {
+        String str;
+        Scanner sc=new Scanner(System.in);
+        str=sc.nextLine();
+        System.out.print(str.toLowerCase());
+    }
+}
