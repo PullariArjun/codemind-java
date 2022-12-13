ab2ec3c7ccd3fec3b1af4118eaf27cccfa9683b3
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-12-13 15:03:21 +0530

    import java.util.Scanner; class arjun{     public static void main(String ram[]){         long n,m;         Scanner sc=new Scanner(System.in);         n=sc.nextLong();         m=sc.nextLong();         System.out.print(n*m);
        }
    }

---
+++ Multiply_Strings.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class arjun{
+    public static void main(String ram[]){
+        long n,m;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextLong();
+        m=sc.nextLong();
+        System.out.print(n*m);
+        
+    }
+}
