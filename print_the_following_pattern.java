ebcbe3b0ab239c38afee8bdc7bb4f88d4e7ecfd2
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2023-02-04 11:28:14 +0530

    import java.util.Scanner; class arjun{     public static void main(String ram[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int i,j;         for(i=1;i<=n;i++){             for(j=1;j<=n-i;j++)                 System.out.print(" ");             for(j=1;j<=(n*2)-(n-i)*2-1;j++)                 System.out.print(i);             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
-class arjun
-{
-    public static void main(String ram[])
-    {
-        int n,i,j;
+class arjun{
+    public static void main(String ram[]){
         Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        int x=n+64;
-        for(i=n;i>0;i--)
-        {
-            for(j=1;j<=i;j++)
-            {
-                System.out.print((char)x+" ");
-            }
-            x--;
-            System.out.print("
-");
+        int n=sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++)
+                System.out.print(" ");
+            for(j=1;j<=(n*2)-(n-i)*2-1;j++)
+                System.out.print(i);
+            System.out.println();
         }
     }
 }
